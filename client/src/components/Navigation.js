d3cfb01d017a3e0c746281705d24ccf5361e3921
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-21 20:59:26 +0530

    Remove Nav Link Index

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -10,21 +10,11 @@ const Navigation = () => (
     <div className="navigation__background">&nbsp;</div>
     <nav className="navigation__nav">
       <ul className="navigation__list">
-        <NavLinkItem href="/" id={1}>
-          home
-        </NavLinkItem>
-        <NavLinkItem href="/about" id={2}>
-          about
-        </NavLinkItem>
-        <NavLinkItem href="/work" id={3}>
-          work
-        </NavLinkItem>
-        <NavLinkItem href="/contact" id={4}>
-          contact
-        </NavLinkItem>
-        <NavLinkItem href="/resume" id={5}>
-          resume
-        </NavLinkItem>
+        <NavLinkItem href="/">home</NavLinkItem>
+        <NavLinkItem href="/about">about</NavLinkItem>
+        <NavLinkItem href="/work">work</NavLinkItem>
+        <NavLinkItem href="/contact">contact</NavLinkItem>
+        <NavLinkItem href="/resume">resume</NavLinkItem>
       </ul>
     </nav>
   </div>
--- client/src/components/navigation/NavLinkItem.js
+++ client/src/components/navigation/NavLinkItem.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-const NavLinkItem = ({ href, id, children }) => (
+const NavLinkItem = ({ href, children }) => (
   <li className="navigation__item">
     <a className="navigation__link" href={href}>
-      <span>{String(id).padStart(2, "0")}</span>
       {children}
     </a>
   </li>
